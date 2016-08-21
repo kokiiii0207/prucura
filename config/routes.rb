9d6f11eaf55f45d4214880dd5a38dffa81a30a30
kokiiii0207 <kokiiii12189527@gmail.com>
2016-08-21 16:39:29 +0900

    reservationモデルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'reservations#index'
+  root to: 'reservations#index', :as => :reservations_index
+
+  resources :reservations, except: [:index, :show] do
+    collection do
+      get  :form
+    end
+  end
 end
