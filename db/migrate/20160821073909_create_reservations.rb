b0f796a8e328446e197d2d9e96b7948b92e4d8de
kokiiii0207 <kokiiii12189527@gmail.com>
2016-08-21 16:54:27 +0900

    reservationテーブルの追加

--- db/migrate/20160821073909_create_reservations.rb
+++ db/migrate/20160821073909_create_reservations.rb
@@ -1,7 +1,14 @@
 class CreateReservations < ActiveRecord::Migration
   def change
     create_table :reservations do |t|
-
+      t.string :last_name
+      t.string :first_name
+      t.text :email
+      t.string :tel
+      t.string :line_id
+      t.string :day_1
+      t.string :day_2
+      t.string :day_3
       t.timestamps null: false
     end
   end
